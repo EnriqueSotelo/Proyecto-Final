bddc005f10ac92ae9c73187f98f7e541314fe898
EnriqueSotelo <90064984+EnriqueSotelo@users.noreply.github.com>
2022-06-06 13:37:21 -0600

    Update Bala.cpp

--- Bala.cpp
+++ Bala.cpp
@@ -1,113 +1,53 @@
-#include "Enemigo.h"
+#include "disparos.h"
+#include <allegro.h>
 
-void Enemigo::construirNave(char* rutaNave, char* rutaBala, char* rutaDisparo, char* rutaEscudo, char* rutaExplosion, int anchoBala, int altoBala,
-                                int anchoPersonaje, int altoPersonaje, int posNavX, int posNavY, int direccionBala, int tip, int vid){
+void crear_bala(int& n_disparos, const int max_disparos ,struct Balas disparos[] ,
+                const int X, const int Y , const int dy)
+{
 
-    posNaveX = posNavX;
-    posNaveY = posNavY;
-    max_disp = 2;
-    nDisparos = 0;
-    tick = 0;
-    ancho_b = anchoBala;
-    alto_b = altoBala;
-    ancho_p = anchoPersonaje;
-    alto_p = altoPersonaje;
-    direccion = direccionBala;
-    tipo = tip;
-    vidas = vid;
+              if( n_disparos < max_disparos-1){ // n -1 disparos permitidos
+                   n_disparos++;
 
-
-    imgNave = load_bitmap(rutaNave, NULL);
-    imgBala = load_bitmap(rutaBala, NULL);
-    imgExplosion = load_bitmap(rutaExplosion, NULL);
-    disparo = load_wav(rutaDisparo);
-    escudo = load_wav(rutaEscudo);
-    explosivo = load_wav(rutaExplosion);
-}
-
-
-void Enemigo::pintar(BITMAP* buffer, int ix, int iy){
-    masked_blit(imgNave, buffer, ix * ancho_p, iy * alto_p , posNaveX, posNaveY, ancho_p, alto_p);
-}
-
-
-bool Enemigo::temporizador(){
-    tick++;
-    if (tick == 5){
-        tick = 0;
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-
-
-void Enemigo::disparar(struct Bala disparos[], BITMAP* buffer){
-    if (tipo)
-    if (crear_bala(nDisparos, max_disp, disparos, posNaveX, posNaveY, direccion)){
-        play_sample(disparo, 175, 127, 1000, 0);
-    }
-    pintar_bala(nDisparos, max_disp, disparos, buffer, imgBala, ancho_b, alto_b);
-    elimina_bala(nDisparos, max_disp, disparos, 600, 600);
+                   disparos[n_disparos].x  = X+11;
+                   disparos[n_disparos].y  = Y;
+                   disparos[n_disparos].dx = 0;
+                   disparos[n_disparos].dy = dy;
+               }
 
 }
 
-void Enemigo::explosion(BITMAP* buffer){
-    if (this->vidas == 0){
-        play_sample(escudo, 127, 127, 1000, 0);
-    }
-    else {
-        play_sample(explosivo, 127, 127, 1000, 0);
-    }
-    BITMAP *expMedio = create_bitmap(25, 20);
-    clear_to_color(expMedio, 0x000000);
-    blit(expMedio, buffer, 0, 0, posNaveX, posNaveY, 25, 20);
-    masked_blit(imgExplosion, buffer, 0, 0, posNaveX - 10, posNaveY, 41, 34);
-}
+void pintar_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
+                 BITMAP* buffer, BITMAP* bala, int ancho, int alto)
+{
 
-void Enemigo::moverEnemigos(Enemigo e[], int& mov, int& dir){
-    for (int i = 0; i < 55; i++) {
-        e[i].posNaveX += dir;
-    }
-    if (++mov == 2){
-        mov = 0;
-    }
-   if (bordeTablero(e, dir)){
-        for (int i = 0; i < 55; i++){
-            e[i].posNaveY += 10;
-        }
-    }
-}
-
-void Enemigo::insertarEnemigos(Enemigo e[], int& vidas){
-    int indice = -1;
-    int tipo = 0;
-    for (int i = 0; i < 5; i++){
-        tipo++;
-        if (tipo == 4){
-
-            tipo = 1;
-        }
-        for (int j = 0; j < 11; j++){
-            indice++;
-            e[indice].construirNave((char*)"Recursos/enemigos.bmp", (char*)"Recursos/Bala_enem.bmp",
-                                    (char*)"Recursos/soundEffects_Disparo.wav",  (char*)"Recursos/soundEffects_Explosion.wav",
-                                    (char*)"Recursos/soundEffects_ExplosionShield.wav", 6, 12, 25, 20, 140 + j * 30, 130 + i * 24, 8, tipo, vidas);
-        }
-    }
-}
-
-
-void Enemigo::pintarEnemigo(Enemigo e[], BITMAP * buffer, int mov){
-    int indice = -1;
-    for (int i = 0; i < 5; i++){
-        for (int j = 0; j < 11; j++){
-            indice++;
-            if (e[indice].vidas > 0){
-                e[indice].pintar(buffer, mov, e[indice].tipo - 1);
+     if ( n_disparos > 0 && n_disparos < max_disparos){
+            for ( int cont = 1; cont <= n_disparos; cont++){
+                       disparos[cont].x += disparos[cont].dx;
+                       disparos[cont].y += disparos[cont].dy;
+                       masked_blit( bala, buffer, 0, 0, disparos[cont].x, disparos[cont].y, ancho, alto );
             }
-        }
-    }
+     }
+
+}
+
+void elimina_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
+                  const int ANCHO, const int ALTO)
+{
+
+      Balas Btemp;
+      if ( n_disparos > 0 && n_disparos < max_disparos){
+            for ( int cont = 1; cont <= n_disparos; cont++){
+                      if ( disparos[cont].y > ALTO || disparos[cont].y < 0 ||
+                           disparos[cont].x > ANCHO|| disparos[cont].x < 0  )
+                       {
+                                Btemp = disparos[cont];
+                                disparos[cont] = disparos[n_disparos];
+                                disparos[n_disparos] = Btemp;
+                                n_disparos--;
+                                if ( n_disparos < 0 ) n_disparos=0;
+                       }
+            }
+      }
+
+
 }
