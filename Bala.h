7cfa6939ab204bede40e7ee1d781be876d7e0d6b
EnriqueSotelo <90064984+EnriqueSotelo@users.noreply.github.com>
2022-06-06 13:38:18 -0600

    Update Bala.h

--- Bala.h
+++ Bala.h
@@ -1,32 +1,19 @@
-#ifndef BALA_H_INCLUDED
-#define BALA_H_INCLUDED
 #include <allegro.h>
-#include "Enemigo.h"
-#include "Escudos.h"
-#include "Nave.h"
+#ifndef DISPAROS_H_INCLUDED
+#define DISPAROS_H_INCLUDED
 
-/*Creamos la estructura que tendra la bala por ejemplo las coordenadas y la anchura*/
-struct Bala{
+struct Balas{
     int x , y;
     int dx , dy;
 
+};
 
-bool choque(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
-
-bool crear_bala(int& n_disparos, const int max_disparos, struct Bala disparos[],
+void crear_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
                 const int X, const int Y ,const int dy);
 
+void pintar_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
+                 BITMAP* buffer, BITMAP* bala, int ancho, int alto );
 
-void pintar_bala(int& n_disparos, const int max_disparos, struct Bala disparos[],
-                 BITMAP* buffer, BITMAP* bala, int ancho, int alto);
-
-void eliminar(struct Bala disparos[], int& nDiparos, int cont);
-void elimina_bala(int& n_disparos, const int max_disparos, struct Bala disparos[],
+void elimina_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
                   const int ANCHO, const int ALTO);
-bool eliminar_bala_choque(struct NAVE& n, struct Enemigo& e, Bala b[]);
-bool eliminar_bala_choque(struct Enemigo& n, struct NAVE& e, Bala b[]);
-void eliminar_bala_escudo(struct NAVE& n, struct Escudos ES[], Bala b[]);
-void eliminar_bala_escudo(struct Enemigo& n, struct Escudos ES[], Bala b[]);
-
-};
 #endif // BALA_H_INCLUDED
