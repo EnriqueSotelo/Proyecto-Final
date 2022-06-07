86fc92613a12c13c2b904ec66c04704a32292de9
EnriqueSotelo <90064984+EnriqueSotelo@users.noreply.github.com>
2022-06-06 20:44:37 -0600

    Update Bala.h

--- Bala.h
+++ Bala.h
@@ -1,12 +1,17 @@
 #include <allegro.h>
+#include "personajes.h"
 #ifndef DISPAROS_H_INCLUDED
 #define DISPAROS_H_INCLUDED
 
+
 struct Balas{
     int x , y;
-    int dx , dy;
+    int dx, dy;
 
 };
+
+bool colicion(int x1, int y1, int w1, int h1,
+              int x2, int y2, int w2, int h2);
 
 void crear_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
                 const int X, const int Y ,const int dy);
@@ -14,6 +19,12 @@ void crear_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
 void pintar_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
                  BITMAP* buffer, BITMAP* bala, int ancho, int alto );
 
+void eliminar (struct Balas disparos[], int& n_disparos, int cont);
+
 void elimina_bala(int& n_disparos, const int max_disparos,struct Balas disparos[],
                   const int ANCHO, const int ALTO);
-#endif // BALA_H_INCLUDED
+
+void elimina_bala_objeto(struct NAVE& N, struct NAVE& E, struct Balas B[]);
+
+#endif // DISPAROS_H_INCLUDED
+
