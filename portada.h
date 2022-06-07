fe7b4d2fd0ff3e885911f23b2b0b7d461808150a
EnriqueSotelo <90064984+EnriqueSotelo@users.noreply.github.com>
2022-06-06 20:47:52 -0600

    Create portada.h

---
+++ portada.h
@@ -0,0 +1,11 @@
+#include <allegro.h>
+#ifndef PORTADA_H_INCLUDED
+#define PORTADA_H_INCLUDED
+
+
+void PORTADA(BITMAP* portada);
+void imprimir_fondo(BITMAP* fondo, BITMAP* buffer);
+
+
+
+#endif // PORTADA_H_INCLUDED
